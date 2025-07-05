02d5e056193f29268dd98989bdf256a70397bc64
kashifashah1 <kashifa.shah24@gmail.com>
2025-07-05 22:10:33 +0530

    React functional component Added

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCasing
+function Message(){
+    //jsx: javascript XML
+    const name='';
+    if(name)
+     return <h1>Hello {name}</h1>;
+    return <h1>hello world</h1>;
+
+}
+
+export default Message;
